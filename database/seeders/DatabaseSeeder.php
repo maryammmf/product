1a327cf17684b3ff2e50dc0177caa9ee53e5e943
maryam <maryammmf@gmail.com>
2024-02-16 15:09:46 +0330

    [feat] added product seeder and factory

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use App\Models\Post;
+use App\Models\Product;
 use App\Models\Tag;
 use App\Models\User;
 use Illuminate\Database\Seeder;
@@ -18,7 +19,8 @@ class DatabaseSeeder extends Seeder
         $this->call([
             UserSeeder::class,
             PostSeeder::class,
-            TagSeeder::class
+            TagSeeder::class,
+            ProductSeeder::class
         ]);
     }
 }
